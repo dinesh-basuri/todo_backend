0b43e92e883635c2f44efb272b57208d3272c0ad
Dinesh Kumar <132756257+dinesh-basuri@users.noreply.github.com>
2025-08-05 10:11:25 +0530

    Update taskRoutes.js

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -3,7 +3,7 @@ const Router = express.Router()
 const taskController = require('../controllers/taskController')
 
 Router.route('/').get(taskController.getTaskList).post(taskController.createTask)
-Router.route('/:id').get(taskController.getTaskById)
+// Router.route('/:id').get(taskController.getTaskById)
 Router.route('/delete').post(taskController.deleteTask)
 Router.route('/update').post(taskController.updateTask)
 
